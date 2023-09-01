468e367a17d1c3a06d65be25544aed49379577bd
Pigiotyreal <pigiotyreal@gmail.com>
2023-09-01 09:57:00 -0400

    checking for args

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "One argument is required." << std::endl;
+        return 1;
+    }
+
+    std::string arg = argv[1];
+    if (arg == "html") {
+        std::cout << "htmllll" << std::endl;
+    }
+
     std::cout << "Hello world!" << std::endl;
     return 0;
 }
