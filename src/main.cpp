00deac02d1fd80b6026cef3c8a0172f358be95b4
Pigiotyreal <pigiotyreal@gmail.com>
2023-09-01 10:19:41 -0400

    node

--- src/main.cpp
+++ src/main.cpp
@@ -33,5 +33,26 @@ int main(int argc, char* argv[]) {
         std::ofstream file("index.js");
         file << "console.log(\"Hello world!\")";
         file.close();
+    } else if(arg == "node" || arg == "nodejs") {
+        std::string command = "mkdir src";
+        system(command.c_str());
+        std::ofstream file("src/index.js");
+        file << "console.log(\"Hello world!\")";
+        file.close();
+        std::ofstream file2("package.json");
+        file2 << "{\n"
+            << "\t\"name\": \"my-node-project\",\n"
+            << "\t\"version\": \"1.0.0\",\n"
+            << "\t\"description\": \"This is my node project!\",\n"
+            << "\t\"main\": \"src/index.js\",\n"
+            << "\t\"scripts\": {\n"
+            << "\t\t\"start\": \"node src/index.js\"\n"
+            << "\t},\n"
+            << "\t\"keywords\": [],\n"
+            << "\t\"author\": \"\",\n"
+            << "\t\"license\": \"ISC\"\n"
+            << "}";
+        file2.close();
+        return 0;
     }
 }
